cefc06a4c90cd75a303699e173c495ee6de8ec40
KiriRainCat <2287990465@qq.com>
2024-02-07 18:36:18 +0800

    ✨ feat: 漏掉的语言文件

--- lib/i18n/zh_Hans.dart
+++ lib/i18n/zh_Hans.dart
@@ -1,5 +1,7 @@
 const Map<String, String> zh_Hans = {
   ..._words,
+  ..._phrases,
+  ..._segments,
 };
 
 const Map<String, String> _words = {
@@ -8,4 +10,21 @@ const Map<String, String> _words = {
   "username": "用户名",
   "email": "邮箱",
   "password": "密码",
+  "register": "注册",
+  "validationCode": "验证码",
+  "length": "长度",
+  "send": "发送"
+};
+
+const Map<String, String> _phrases = {
+  "noAccountYet": " 还没有账号？",
+  "alreadyHasAccount": "已经有账号了？",
+  "passwordDoesNotMatch": "密码不匹配",
+  "emailInvalid": "邮箱格式错误",
+};
+
+const Map<String, String> _segments = {
+  "cantBeEmpty": "不能为空",
+  "cantBeLessThan": "不能小于",
+  "mustEqualTo": "必须等于",
 };
